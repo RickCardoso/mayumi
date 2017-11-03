7ce76968ccbbdb7dbe60ec10c73518a16779d08d
Ricardo Cardoso <rickclsilva@gmail.com>
2017-11-03 14:15:49 -0200

    animations and icons
    animate with emergence.js
    icons added in png

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
 
+  emergence.init({
+    container: null,
+    throttle: 250,
+    reset: true,
+    handheld: true,
+    elemCushion: 0.15,
+    offsetTop: 0,
+    offsetRight: 0,
+    offsetBottom: 0,
+    offsetLeft: 0,
+    callback: function(element, state) {
+      if (state === 'visible') {
+        console.log('Element is visible.');
+      } else if (state === 'reset') {
+        console.log('Element is hidden with reset.');
+      } else if (state === 'noreset') {
+        console.log('Element is hidden with NO reset.');
+      }
+    }
+  });
+  window.scrollBy(0, 1);
+  window.scrollBy(0, -1);
+
   // smooth scrolling
     $("a").click(function(event) {
       if (this.hash !== "") {
